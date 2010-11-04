f97c2d6f388709f1f754737ac6bae540d52df03f
miaosun88 <miaosun88@9ca0f86a-2074-76d8-43a5-19cf18205b40>
2010-11-04 21:30:22 +0000

    Em visual studio, Esta é a versão mais nova! Esquece daquele d eclipse, para já trabalho em Visual Studio 2010, depois usamos eclipse, visual studio tem um mal é que o ficheiro executével não se corre nos pcs que não tem biblioteca d visual studio instalado...

---
+++ Nova Versao, em Visual Studio 2010/Exame.h
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////
+//  Exame.h
+//  Implementation of the Class Exame
+//  Created on:      24-Out-2010 20:04:10
+//  Original author: Answer
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_C770FA25_5477_45dd_BD95_171CA4C42559__INCLUDED_)
+#define EA_C770FA25_5477_45dd_BD95_171CA4C42559__INCLUDED_
+
+#include "Marcacao.h"
+
+class Exame : public Marcacao
+{
+
+public:
+	Exame();
+	virtual ~Exame();
+
+	Exame(string data, string hora, string tipo, string sala);
+	string getSala();
+	void setSala(string sala);
+	string toString();
+	vector<string> imprime();
+	vector<string> editMarcacao();
+
+private:
+	string sala;
+
+};
+#endif // !defined(EA_C770FA25_5477_45dd_BD95_171CA4C42559__INCLUDED_)
