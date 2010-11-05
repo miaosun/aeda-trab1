b7964d1fce355e59a401f246bcbb1ba8e83cc075
gasparlafurtado <gasparlafurtado@9ca0f86a-2074-76d8-43a5-19cf18205b40>
2010-11-05 04:53:54 +0000

    resolucao do problema

---
+++ Versao FIX/Medico.cpp
@@ -0,0 +1,122 @@
+///////////////////////////////////////////////////////////
+//  Medico.cpp
+//  Implementation of the Class Medico
+//  Created on:      24-Out-2010 20:04:14
+///////////////////////////////////////////////////////////
+
+#include "Medico.h"
+
+
+Medico::Medico(){}
+
+Medico::~Medico(){}
+
+Medico::Medico(string nome, string dataNascimento, string tipo, string especialidade, string horario, double vencimento):Pessoa(nome, dataNascimento, tipo)
+{
+	this->especialidade = especialidade;
+	this->horario = horario;
+	this->vencimento = vencimento;
+	this->func = 0;
+}
+
+string Medico::getEspecialidade(){
+
+	return this->especialidade;
+}
+
+void Medico::setEspecialidade(string especialidade)
+{
+	this->especialidade = especialidade;
+}
+
+string Medico::getHorario(){
+
+	return this->horario;
+}
+
+void Medico::setHorario(string horario)
+{
+	this->horario = horario;
+}
+
+double Medico::getVencimento(){
+
+	return this->vencimento;
+}
+
+void Medico::setVencimento(double vencimento)
+{
+	this->vencimento = vencimento;
+}
+
+Funcionario *Medico::getFuncionario()
+{
+	return this->func;
+}
+
+void Medico::setFuncionario(Funcionario *func)
+{
+	this->func = func;
+}
+
+vector<string> Medico::imprime(){
+
+	stringstream ss1,ss2,ss3,ss4;
+	vector<string> v;
+	v=Pessoa::imprime();
+
+	ss1<<"   Especialidade: "<<this->especialidade;
+	v.push_back(ss1.str());
+
+	ss2<<"   Horario: "<<this->horario;
+	v.push_back(ss2.str());
+
+	ss3<<"   Vencimento: "<<this->vencimento;
+	v.push_back(ss3.str());
+
+	ss4<<"   Funcionario Associado(ID, nome): ";
+	if(this->func==0){
+		ss4<<" Nao tem.";
+		system("pause");}
+	else{
+		ss4<<this->func->getId()<<", "<<this->func->getName();}
+	v.push_back(ss4.str());
+	return v;
+}
+
+vector<string> Medico::editPessoa()
+{
+	vector<string> opcoes;
+	opcoes = Pessoa::editPessoa();
+	stringstream ss1,ss2,ss3,ss4;
+
+	ss1<<"3 - Editar Especialidade: "<<this->especialidade;
+	opcoes.push_back(ss1.str());
+	ss2<<"4 - Editar Horario: "<<this->horario;
+	opcoes.push_back(ss2.str());
+	ss3<<"5 - Editar Vencimento: "<<this->vencimento;
+	opcoes.push_back(ss3.str());
+	ss4<<"6 - Editar o Funcionario associado: ";
+	if(this->func==0){
+		ss4<<" Nao tem.";
+		system("pause");}
+	else{
+		ss4<<this->func->getId()<<", "<<this->func->getName();}
+
+	opcoes.push_back(ss4.str());
+
+	return opcoes;
+}
+
+string Medico::toString()
+{
+	stringstream ss;
+	ss<<Pessoa::toString()<<this->especialidade<<"|"<<this->horario<<"|"<<this->vencimento<<"|";
+	return ss.str();
+}
+
+
+//funcoes abstradas para objecto da superclasse consegue acessar os metodos das classes derivadas
+void Medico::setMorada(string morada){}
+void Medico::setCargo(string cargo){}
+string Medico::getCargo(){return "";}
---
+++ Versao FIX/Pessoa.cpp
@@ -0,0 +1,110 @@
+///////////////////////////////////////////////////////////
+//  Pessoa.cpp
+//  Implementation of the Class Pessoa
+//  Created on:      24-Out-2010 20:04:15
+///////////////////////////////////////////////////////////
+
+#include "Pessoa.h"
+
+int Pessoa::count = 0;
+
+Pessoa::Pessoa(){}
+
+Pessoa::~Pessoa(){}
+
+Pessoa::Pessoa(string nome, string dataNascimento, string tipo){
+
+	this->id = ++count;
+	this->nome = nome;
+	this->dataNascimento = dataNascimento;
+	this->tipo = tipo;
+}
+
+string Pessoa::getName(){
+
+	return  this->nome;
+}
+
+void Pessoa::setName(string nome)
+{
+	this->nome = nome;
+}
+
+string Pessoa::getDataNascimento(){
+
+	return  this->dataNascimento;
+}
+
+void Pessoa::setDataNascimento(string dataNascimento)
+{
+	this->dataNascimento = dataNascimento;
+}
+
+string Pessoa::getTipo(){
+
+	return  this->tipo;
+}
+
+void Pessoa::setTipo(string tipo){
+	this->tipo = tipo;
+}
+
+int Pessoa::getId()
+{
+	return this->id;
+}
+
+vector<string> Pessoa::imprime()
+{
+	vector<string> v;
+	stringstream s1, s2, s3, s4;
+
+	s1<<this->tipo<<":";
+	v.push_back(s1.str());
+
+	s2<<"   ID: "<<this->id;
+	v.push_back(s2.str());
+
+	s3<<"   Nome: "<<this->nome;
+	v.push_back(s3.str());
+
+	s4<<"   Data de Nascimento: "<<this->dataNascimento;
+	v.push_back(s4.str());
+
+	return v;
+}
+
+string Pessoa::toString()
+{
+	stringstream ss;
+	ss<<"|"<<this->id<<"|"<<this->nome<<"|"<<this->dataNascimento<<"|"<<this->tipo<<"|";
+	return ss.str();
+}
+
+
+string Pessoa::toList()
+{
+    ostringstream ss;
+    ss<<"|";
+    if (id<10)
+        ss<<" ";
+	ss<<this->id<<"| "<<this->nome<<" |";
+
+    return ss.str();
+}
+
+vector<string> Pessoa::editPessoa()
+{
+	vector<string> opcoes;
+	stringstream ss1,ss2,ss3;
+	opcoes.push_back("Escolha o que editar:");
+	opcoes.push_back("");
+	ss1<<"1 - Editar Nome: "<<this->nome;
+	opcoes.push_back(ss1.str());
+	ss2<<"2 - Editar Data de Nascimento: "<<this->dataNascimento;
+	opcoes.push_back(ss2.str());
+/*	ss3<<"3 - Editar Tipo: "<<this->tipo;
+	opcoes.push_back(ss3.str());*/
+
+	return opcoes;
+}
